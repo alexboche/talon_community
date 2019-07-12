f076d557ac2448f092431fa25309aea962055575
alex <alex@alexs-MacBook-Pro.local>
2019-07-11 20:52:38 -0700

    Revert "window switching working"
    This reverts commit 1fbf895eea2ca883e45a3b2233a4caf9c9aef8be.

--- misc/switcher.py
+++ misc/switcher.py
@@ -55,12 +55,10 @@ ctx.keymap(
         "fox skype": lambda x: switch_app(x, "Skype for Business"),
         "fox signal": lambda x: switch_app(x, "Signal"),
         "system preferences": lambda x: switch_app(x, "System Preferences"),
-        # "fox lyx":
-
     }
 )
 
-hardcoded_application_names = {"term": "iTerm2", "code": "code", "ack": "Acrobat Reader"}
+hardcoded_application_names = {"term": "iTerm2", "code": "code", "ack": "Acrobat Reader", "Lyx", "Lyx"}
 
 
 def update_lists():
