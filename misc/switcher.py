68e35a32ceaacdf6c26bbc73d667dd5fc68a57e8
alex <alex@alexs-MacBook-Pro.local>
2019-07-11 22:56:54 -0700

    improve switching

--- misc/switcher.py
+++ misc/switcher.py
@@ -55,11 +55,12 @@ ctx.keymap(
         "fox skype": lambda x: switch_app(x, "Skype for Business"),
         "fox signal": lambda x: switch_app(x, "Signal"),
         "system preferences": lambda x: switch_app(x, "System Preferences"),
+        "fox mail": lambda x: switch_app(x, "Outlook"),
     }
 )
 
-hardcoded_application_names = {"term": "iTerm2", "code": "code", "ack": "Acrobat Reader", "Lyx", "Lyx"}
-
+# hardcoded_application_names = {"term": "iTerm2", "code": "code", "ack": "Acrobat Reader", "Lyx", "Lyx"}
+hardcoded_application_names = {"term": "iTerm2", "code": "code", "ack": "Acrobat Reader"}
 
 def update_lists():
     global running
