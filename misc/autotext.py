c570795eb61ef406d5082bcee5ca38184ac36bb1
alex <akboche@gmail.com>
2019-08-23 14:07:05 -0700

    dosh

---
+++ misc/autotext.py
@@ -0,0 +1,9 @@
+
+from talon.voice import Context, Key, Str
+from talon import ctrl, clip, resource, ui
+
+ctx = Context("autotext")
+ctx.keymap(
+    {"(student|university) id": "3044905",}
+)
+
---
+++ misc/latex.py
@@ -0,0 +1,5 @@
+from talon.voice import Key, press, Str, Context, ContextGroup
+from talon.voice import Context, Rep, RepPhrase, talon
+from .. import utils
+
+latex_group = ContextGroup("latex")
