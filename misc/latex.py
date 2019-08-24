2534a358292462f2b95f2e2968373066b4d7e86a
alex <akboche@gmail.com>
2019-08-24 02:50:35 -0700

    multi context latex

--- misc/latex.py
+++ misc/latex.py
@@ -2,4 +2,10 @@ from talon.voice import Key, press, Str, Context, ContextGroup
 from talon.voice import Context, Rep, RepPhrase, talon
 from .. import utils
 
-latex_group = ContextGroup("latex")
+# latex_group = ContextGroup("latex")
+latex_ctx = Context("latex", func= lambda app, win: app.bundle in ["com.sublimetext.3", "texmaker"])
+keymap = {"color box": ["\\colorbox{gray}{}", Key('left')],
+"footnote": ["\\footnote{}", Key('left')],}
+
+latex_ctx.keymap(keymap)
+  
